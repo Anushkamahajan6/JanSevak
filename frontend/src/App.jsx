6033938b24298e98cbbc1a9ebe5026da868dfc52
KinjalGupta46 <kinjalgupta)38@gmailcom>
2026-04-12 03:32:24 +0530

    user dashboard routes updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,31 @@ import AdminPage from "./pages/AdminPage";
 import VolunteerPage from "./pages/VolunteerPage";
 import Dashboard from "./pages/user-dashboard/Dashboard";
 import AdminHeatmap from "./Components/AdminHeatmap";
-import heatmapview from "./Components/heatmapview";
+import HeatmapView from "./components/HeatmapView";
+import MyIssues from "./pages/user-dashboard/MyIssues";
+import ReportIssue from "./pages/user-dashboard/ReportIssue";
+import Rewards from "./pages/user-dashboard/Rewards";
+import Profile from "./pages/user-dashboard/Profile";
+import Settings from "./pages/user-dashboard/Settings";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/volunteer" element={<VolunteerPage />} />
-        <Route path="/user" element={<Dashboard />} />
-        <Route path="/admin/heatmap" element={<AdminHeatmap />} />
-        <Route path="/heatmap" element={<heatmapview />} />
-      </Routes>
-    </BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/admin" element={<AdminPage />} />
+    <Route path="/volunteer" element={<VolunteerPage />} />
+    <Route path="/user" element={<Dashboard />} />
+    <Route path="/admin/heatmap" element={<AdminHeatmap />} />
+    <Route path="/heatmapView" element={<HeatmapView />} />
+    <Route path="/user/my-issues" element={<MyIssues />} />
+    <Route path="/user/report" element={<ReportIssue />} />
+    <Route path="/user/rewards" element={<Rewards />} />
+    <Route path="/user/profile" element={<Profile />} />
+    <Route path="/user/settings" element={<Settings />} />
+  </Routes>
+</BrowserRouter>
   );
 }
 
