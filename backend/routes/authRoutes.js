bcd4417a19832ad97ef7a2d9ccd49fa14d6b455a
Iksha Malhotra <iksha16005@gmail.com>
2026-06-10 00:06:51 +0530

    volunteer authentication token flow corrected

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,6 +12,8 @@ router.get("/test", (req, res) => {
 router.post("/logout", (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
     expires: new Date(0), // expire immediately
   });
   res.status(200).json({ message: "Logged out" });
@@ -37,9 +39,9 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
     res.status(200).json({
